36f3f9ac7fb540895d3e873e93aa502fdf3b89c9
Sourabh Berde <sourabh.berde44@gmail.com>
2022-05-16 15:43:14 +0530

    Initial project setup

---
+++ src/main/java/com/ledger/co/App.java
@@ -0,0 +1,7 @@
+package com.ledger.co;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world..!!");
+    }
+}
---
+++ src/test/java/com/ledger/co/AppTest.java
@@ -0,0 +1,14 @@
+package com.ledger.co;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
